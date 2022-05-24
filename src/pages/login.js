17d429f0640ca1319190f3c26c9f99c611622ad9
davidsonDeCarvalho <davidsonccarvalho@gmail.com>
2022-05-24 16:39:55 -0300

    Requisito 01

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Login extends Component {
+class Login extends React.Component {
+  state = {
+    user: '',
+    email: '',
+  }
+
+  handleChange = ({ target }) => {
+    console.log(target.value);
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
   render() {
+    const { user, email } = this.state;
     return (
-      <p>Login</p>
+      <div>
+        <form>
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="input-gravatar-email"
+          />
+          <input
+            type="text"
+            name="user"
+            value={ user }
+            onChange={ this.handleChange }
+            data-testid="input-player-name"
+          />
+          <button
+            disabled={ email.length === 0 || user.length === 0 }
+            type="submit"
+            data-testid="btn-play"
+          >
+            Play
+          </button>
+        </form>
+      </div>
     );
   }
 }
-
 export default Login;
