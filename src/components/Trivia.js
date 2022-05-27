b398fdf1a4bb05410d2fbcb9841dc5b8d38f5cda
Ewerson Martins <ewersonmrt@gmail.com>
2022-05-27 17:15:40 -0300

    Aplica style nos botoes

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import '../App.css';
 
 class Trivia extends React.Component {
   state = {
     currentQuestion: 0,
     showNextBtn: false,
     nextQuestion: [],
+    styleBtnCorrect: '',
+    styleBtnIncorrect: '',
   }
 
   verifyAnswer = (alternative) => {
     if (alternative === 'correct-answer') {
-      console.log('Correto');
-    } else console.log('Errado');
+      this.setState({
+        styleBtnCorrect: 'btnCorrectOption',
+        styleBtnIncorrect: 'btnIncorrectOption',
+      });
+      console.log('Certo');
+    } else {
+      this.setState({
+        styleBtnCorrect: 'btnCorrectOption',
+        styleBtnIncorrect: 'btnIncorrectOption',
+      });
+      console.log('Errado');
+    }
     this.setState({ showNextBtn: true });
   }
 
@@ -19,38 +32,36 @@ class Trivia extends React.Component {
     const { resultsQuestions } = this.props;
     const { currentQuestion } = this.state;
     const { results } = resultsQuestions;
-
     // const answers = [results[0].correct_answer, ...results[0].incorrect_answers];
-
-    const alternatives = results[currentQuestion + 1].incorrect_answers.map((text) => ({
-      text,
-      isCorrect: false,
-    }));
-    alternatives.push({
-      text: results[currentQuestion + 1].correct_answer,
-      isCorrect: true,
-    });
-    console.log(alternatives);
-
-    const RANDOM_CONST = 0.5;
-    const nextQuestion = alternatives.sort(() => Math.random() - RANDOM_CONST);
-
-    this.setState({
-      nextQuestion,
-    });
-
-    console.log('results', results);
-    return nextQuestion;
+    if (currentQuestion < results.length - 1) {
+      const alternatives = results[currentQuestion + 1].incorrect_answers.map((text) => ({
+        text,
+        isCorrect: false,
+      }));
+      alternatives.push({
+        text: results[currentQuestion + 1].correct_answer,
+        isCorrect: true,
+      });
+      const RANDOM_CONST = 0.5;
+      const nextQuestionRandom = alternatives.sort(() => Math.random() - RANDOM_CONST);
+      this.setState({
+        nextQuestion: nextQuestionRandom,
+      });
+      console.log('results', results);
+      console.log('alternatives', alternatives);
+      return nextQuestionRandom;
+    }
   }
 
   nextQuestion = () => {
     const { resultsQuestions, history } = this.props;
     const { currentQuestion } = this.state;
     const { results } = resultsQuestions;
-
     this.setState({ showNextBtn: false });
     if (currentQuestion < results.length - 1) {
-      this.setState({ currentQuestion: currentQuestion + 1 });
+      this.setState({
+        currentQuestion: currentQuestion + 1,
+      });
     } else {
       history.push('/'); // REDIRECIONAR PARA RANKING
     }
@@ -60,13 +71,17 @@ class Trivia extends React.Component {
   }
 
   render() {
-    const { currentQuestion, showNextBtn, nextQuestion } = this.state;
+    const { currentQuestion,
+      showNextBtn,
+      nextQuestion,
+      styleBtnCorrect,
+      styleBtnIncorrect,
+    } = this.state;
     const { resultsQuestions } = this.props;
     const { results = [] } = resultsQuestions;
-
+    console.log('nextQuestion', nextQuestion);
     return (
       <div>
-
         {
           // eslint-disable-next-line no-nested-ternary
           results.length <= 0
@@ -76,9 +91,10 @@ class Trivia extends React.Component {
                 <h1 data-testid="question-category">
                   {results[currentQuestion].category}
                 </h1>
-                <h2 data-testid="question-text">{ results[0].question }</h2>
+                <h2 data-testid="question-text">{ results[currentQuestion].question }</h2>
                 <div data-testid="answer-options">
                   <button
+                    className={ styleBtnCorrect }
                     type="button"
                     data-testid="correct-answer"
                     name="correct-answer"
@@ -88,10 +104,10 @@ class Trivia extends React.Component {
                       results[currentQuestion].correct_answer
                     }
                   </button>
-
                   {results[currentQuestion].incorrect_answers
                     .map((incorretAnsewr, index) => (
                       <button
+                        className={ styleBtnIncorrect }
                         key={ index }
                         type="button"
                         name={ `wrong-answer-${index}` }
@@ -107,9 +123,7 @@ class Trivia extends React.Component {
                 <h1 data-testid="question-category">
                   {results[currentQuestion].category}
                 </h1>
-
                 <h2 data-testid="question-text">{ results[currentQuestion].question }</h2>
-
                 {nextQuestion.map(({ text, isCorrect }, index) => (
                   <div key={ index } data-testid="answer-options">
                     <button
@@ -130,7 +144,6 @@ class Trivia extends React.Component {
             )
         }
         {/* END - // results.length // */}
-
         {showNextBtn && (
           <button
             data-testid="btn-next"
@@ -145,9 +158,7 @@ class Trivia extends React.Component {
     );
   }
 }
-
 const mapStateToProps = (state) => ({
   resultsQuestions: state.gameReducer.resultsQuestions,
 });
-
 export default connect(mapStateToProps)(Trivia);
